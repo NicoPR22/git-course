dad3d08c0d1877bc246439bb89ffc90127e4b02a
NicoPR22 <nicolasexeburgos@gmail.com>
2022-06-17 21:17:16 -0300

    Estructura: creando un proyecto basico de Flask. NB

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-form src.app import sum
-def main()-> None:
-    sum(5,10)
+from src.app import app
+HOST= "localhost"
+PORT= 4000
+DEBUG= True
 
 if __name__ == '__main__':
-    main()
+    app.run(HOST,PORT,DEBUG)
