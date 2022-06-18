d5b382236fa466a146cb2addc0834406057090e6
NicoPR22 <nicolasexeburgos@gmail.com>
2022-06-17 22:41:17 -0300

    Feature: añadiendo saludo por medio de la ruta. NB

--- src/app.py
+++ src/app.py
@@ -1,2 +1,9 @@
+from asyncio import run_coroutine_threadsafe
+from unicodedata import name
 from flask import Flask
-app == Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello!'
+
